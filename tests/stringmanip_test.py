30be060a92fe7770a5d5558fcf8d95fbd13cb84f
Jamison <jamison.p.blanchard@gmail.com>
2017-04-25 20:40:49 -0700

    Write first tests of stringmanip

---
+++ tests/stringmanip_test.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../Intercut')
+
+import pytest
+
+from tools import stringmanip
+
+def test_cut_basic_string():
+    assert "the boy is " == stringmanip.remove_last_word("the boy is cool.")
+
+def test_cut_empty_string():
+    with pytest.raises(ValueError):
+        stringmanip.remove_last_word("")
