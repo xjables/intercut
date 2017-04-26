f1b2212ee95ccab661b119a878b48dcac99c480f
Jamison <jamison.p.blanchard@gmail.com>
2017-04-25 19:50:13 -0700

    Refactor word deletion code into tools.stringmanip

--- Intercut/tools/stringmanip.py
+++ Intercut/tools/stringmanip.py
@@ -1,3 +1,13 @@
 
 
+def remove_last_word(string):
+    """Return input string without the last word.
+    
+    Does not remove trailing whitespace character. A "word" is a collection of
+    characters without spaces.
 
+    Args:
+        string (str): A string.
+    """
+    # TODO: Below is hackey. Do it properly.
+    return string[::-1].split(' ', 1)[1][::-1] + ' '
