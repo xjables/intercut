11c809a004089b6f083859c1ebfca9ab7b699fc4
Jamison <jamison.p.blanchard@gmail.com>
2017-04-26 19:33:47 -0700

    Wrote empty string test for remove_last_word

--- tests/stringmanip_test.py
+++ tests/stringmanip_test.py
@@ -1,10 +1,16 @@
 import pytest
 
-from intercut.Intercut.tools import stringmanip
+from intercut.intercut.tools import stringmanip
+
 
 def test_cut_basic_string():
     assert "the boy is " == stringmanip.remove_last_word("the boy is cool.")
 
+
 def test_cut_empty_string():
     with pytest.raises(ValueError):
         stringmanip.remove_last_word("")
+
+
+def test_cut_no_spaces():
+    assert "" == stringmanip.remove_last_word("NoSpacesInString")
