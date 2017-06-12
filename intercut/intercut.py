397d89474f47625036d13bb53e168c023fbd023d
Jamison Blanchard <jamison.p.blanchard@gmail.com>
2017-06-11 17:59:59 -0700

    Create tabular window view for screenplays

--- intercut/intercut.py
+++ intercut/intercut.py
@@ -1,15 +1,33 @@
+from kivy.core.window import Window
+Window.clearcolor = (.2, .2, .2, 1)
+
 
 from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
 
-from screenplay import ScrollingScreenplay
+from screenplay import Screenplay
+
+Builder.load_file(r'intercut.kv')
 
 
-class MyApp(App):
+class ViewManager(TabbedPanel):
+    pass
+
+
+class ScreenplayManager(TabbedPanel):
+    pass
+
+
+class InterXut(App):
 
     def build(self):
         self.title = "InterXut"
-        return ScrollingScreenplay()
+        spm = ScreenplayManager()
+        spm._tab_strip.padding = [40,0,0,0]
+        print(spm._tab_strip.padding)
+        return spm
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    InterXut().run()
